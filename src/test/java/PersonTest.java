8a1888e8c254477655fd646629375f57ac7b623c
Neha Vats <nehavats2020@gmail.com>
2020-01-19 15:56:09 -0500

    Test Case of First Name is added

--- src/test/java/PersonTest.java
+++ src/test/java/PersonTest.java
@@ -1,3 +1,4 @@
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -6,24 +7,40 @@ import static org.junit.Assert.assertEquals;
 
 public class PersonTest
 {
-
+    Person a1=null;
+    @Before
+    public void setVales()
+    {
+        a1=new Person(40,"Neha","Kumari");
+    }
 
     @Test
     public void setAgeTest()
     {
-        Person a1=new Person(40,"Neha","Kumari");
-
-
-        a1.setAge(45);
+        int expectedOutput=45;
+        a1.setAge(expectedOutput);
         int ActualOutput=a1.getAge();
-        assertEquals(45, ActualOutput);
-
+        assertEquals(expectedOutput, ActualOutput);
     }
-@Test
+    @Test
     public void getAgeTest()
     {
-        Person a2=new Person(50,"Tarun","Vats");
-       int AcctualOutput2= a2.getAge();
-       assertEquals(50,AcctualOutput2);
+        int Expexctedage=40;
+        int AcctualOutput2= a1.getAge();
+        assertEquals(Expexctedage,AcctualOutput2);
+    }
+    @Test
+    public void setFnameTest()
+    {
+        String expectectedFname="Kakku";
+        a1.setFName(expectectedFname);
+        String ActualFname= a1.getFName();
+        assertEquals(expectectedFname, ActualFname);
+    }
+
+    @After
+    public void destroy()
+    {
+        a1=null;
     }
 }
