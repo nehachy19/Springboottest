dc13436702025d56e2190e6b12968089bc7503e2
tarun00000 <tarunvats1990@gmail.com>
2020-01-18 19:53:38 -0500

    Add files via upload

---
+++ src/main/java/Person.java
@@ -0,0 +1,45 @@
+
+public class Person 
+{
+	Integer age;
+	String FName;
+	String Lname;
+	
+	Person(Integer age,String FName,String Lname)
+	{
+		this.age=age;
+		this.FName=FName;
+		this.Lname=Lname;
+	}
+	public void setAge(Integer age)
+	{
+		this.age=age;
+	}
+	
+	public void setFName(String Fname)
+	{
+		this.FName="bejw";
+	}
+	
+	public void setLname(String Lname)
+	{
+		this.Lname=Lname;
+	}
+	
+	
+	public Integer getAge()
+	{
+		return this.age;
+	}
+	
+	public String getFName()
+	{
+		return this.FName;
+	}
+	
+	public String getLname()
+	{
+		return this.Lname;
+	}	
+}
+
